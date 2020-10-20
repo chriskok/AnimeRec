591638bfba0e6e05744bf34c0028e9bbe7d2f93b
Kok <pherkok@gmail.com>
2020-10-19 21:36:40 -0500

    Updated like/dislike buttons

--- front_end_animerec/static/js/app.js
+++ front_end_animerec/static/js/app.js
@@ -213,13 +213,11 @@ function sendQuery(e) {
                 </div>
                 <div class="h-10"></div>
                 <div class="absolute inset-x-0 bottom-0 button-div mb-4 ">
-                  <button type="button" id="likebtn-` + type + `-` + curr_key + `" class="block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" onclick="rateRecs('` + chosen_anime + `','` + animeTitle + `',1, '` + type + `','` + curr_key + `')">
-                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"/></svg>
-                    Like
+                  <button type="button" id="likebtn-` + type + `-` + curr_key + `" class="block bg-transparent py-2 px-4 rounded inline-flex items-center" onclick="rateRecs('` + chosen_anime + `','` + animeTitle + `',1, '` + type + `','` + curr_key + `')">
+                    <i class="material-icons">thumb_up</i>
                   </button>
-                  <button type="button" id="dislikebtn-` + type + `-` + curr_key + `" class="block bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-flex items-center" onclick="rateRecs('` + chosen_anime + `','` + animeTitle + `',0, '` + type + `','` + curr_key + `')">
-                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"/></svg>
-                    Dislike 
+                  <button type="button" id="dislikebtn-` + type + `-` + curr_key + `" class="block bg-transparent py-2 px-4 rounded inline-flex items-center" onclick="rateRecs('` + chosen_anime + `','` + animeTitle + `',0, '` + type + `','` + curr_key + `')">
+                    <i class="material-icons">thumb_down</i> 
                   </button>
                 </div>
               `
