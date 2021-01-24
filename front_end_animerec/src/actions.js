6bc4f71194a6e477f71a4dd9696d6c033f4b0bc3
chriskok <pherkok@gmail.com>
2021-01-24 08:50:57 -0600

    Fixed proxy errors for production

--- front_end_animerec/src/actions.js
+++ front_end_animerec/src/actions.js
@@ -10,21 +10,21 @@ function fetchJson(url, kwds) {
 }
 
 export function getNames() {
-	return fetchJson(`/api/v1/search_info`);
+	return fetchJson(`http://3.131.210.47:8000/api/v1/search_info`);
 }
 
 export function getRecommendations(animeId, quantity = 5) {
 	return fetchJson(
-		`/api/v1/recommendations?anime_code=${animeId}&n_recommendations=${quantity}`
+		`http://3.131.210.47:8000/api/v1/recommendations?anime_code=${animeId}&n_recommendations=${quantity}`
 	);
 }
 
 export function getAnime(animeId) {
-	return fetchJson(`/api/v1/anime?anime_code=${animeId}`);
+	return fetchJson(`http://3.131.210.47:8000/api/v1/anime?anime_code=${animeId}`);
 }
 
 export function getUserReview(animeId, referenceId) {
 	return fetchJson(
-		`/api/v1/user_recommendations?search_anime=${animeId}&recommended_anime=${referenceId}`
+		`http://3.131.210.47:8000/api/v1/user_recommendations?search_anime=${animeId}&recommended_anime=${referenceId}`
 	);
 }
