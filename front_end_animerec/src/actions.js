c1f832607fb603555ad1d11696f46d19acd99517
chriskok <pherkok@gmail.com>
2021-01-29 13:47:21 -0600

    Updated to use docker API container instead

--- front_end_animerec/src/actions.js
+++ front_end_animerec/src/actions.js
@@ -10,21 +10,21 @@ function fetchJson(url, kwds) {
 }
 
 export function getNames() {
-	return fetchJson(`http://3.131.210.47:8000/api/v1/search_info`);
+	return fetchJson(`/api/v1/search_info`);
 }
 
 export function getRecommendations(animeId, quantity = 5) {
 	return fetchJson(
-		`http://3.131.210.47:8000/api/v1/recommendations?anime_code=${animeId}&n_recommendations=${quantity}`
+		`/api/v1/recommendations?anime_code=${animeId}&n_recommendations=${quantity}`
 	);
 }
 
 export function getAnime(animeId) {
-	return fetchJson(`http://3.131.210.47:8000/api/v1/anime?anime_code=${animeId}`);
+	return fetchJson(`/api/v1/anime?anime_code=${animeId}`);
 }
 
 export function getUserReview(animeId, referenceId) {
 	return fetchJson(
-		`http://3.131.210.47:8000/api/v1/user_recommendations?search_anime=${animeId}&recommended_anime=${referenceId}`
+		`/api/v1/user_recommendations?search_anime=${animeId}&recommended_anime=${referenceId}`
 	);
 }
